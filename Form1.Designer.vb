a70da7a1d72c9f3d16d2450e0626101e627f0866
LuisVSa <luis@ptsim.com>
2017-10-13 15:28:20 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ListMapServers = New System.Windows.Forms.ListBox()
+        Me.cmdGetTile = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtLongitude = New System.Windows.Forms.TextBox()
+        Me.txtLatitude = New System.Windows.Forms.TextBox()
+        Me.txtZoom = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureTile = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.CmdClearCache = New System.Windows.Forms.Button()
+        CType(Me.PictureTile, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(296, 21)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(162, 13)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Found Tile Plugins - choose one!"
+        '
+        'ListMapServers
+        '
+        Me.ListMapServers.FormattingEnabled = True
+        Me.ListMapServers.Location = New System.Drawing.Point(299, 41)
+        Me.ListMapServers.Name = "ListMapServers"
+        Me.ListMapServers.Size = New System.Drawing.Size(161, 108)
+        Me.ListMapServers.TabIndex = 17
+        '
+        'cmdGetTile
+        '
+        Me.cmdGetTile.Location = New System.Drawing.Point(387, 251)
+        Me.cmdGetTile.Name = "cmdGetTile"
+        Me.cmdGetTile.Size = New System.Drawing.Size(73, 25)
+        Me.cmdGetTile.TabIndex = 16
+        Me.cmdGetTile.Text = "Get Tile "
+        Me.cmdGetTile.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(299, 241)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Longitude:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(299, 198)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Latitude:"
+        '
+        'txtLongitude
+        '
+        Me.txtLongitude.Location = New System.Drawing.Point(299, 256)
+        Me.txtLongitude.Name = "txtLongitude"
+        Me.txtLongitude.Size = New System.Drawing.Size(75, 20)
+        Me.txtLongitude.TabIndex = 13
+        Me.txtLongitude.Text = "-8.6397"
+        Me.txtLongitude.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtLatitude
+        '
+        Me.txtLatitude.Location = New System.Drawing.Point(299, 214)
+        Me.txtLatitude.Name = "txtLatitude"
+        Me.txtLatitude.Size = New System.Drawing.Size(75, 20)
+        Me.txtLatitude.TabIndex = 12
+        Me.txtLatitude.Text = "41.0092"
+        Me.txtLatitude.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtZoom
+        '
+        Me.txtZoom.Location = New System.Drawing.Point(299, 175)
+        Me.txtZoom.Name = "txtZoom"
+        Me.txtZoom.Size = New System.Drawing.Size(33, 20)
+        Me.txtZoom.TabIndex = 11
+        Me.txtZoom.Text = "12"
+        Me.txtZoom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(299, 159)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Zoom:"
+        '
+        'PictureTile
+        '
+        Me.PictureTile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureTile.Location = New System.Drawing.Point(21, 21)
+        Me.PictureTile.Name = "PictureTile"
+        Me.PictureTile.Size = New System.Drawing.Size(256, 256)
+        Me.PictureTile.TabIndex = 19
+        Me.PictureTile.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(27, 41)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 26)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Error!"
+        '
+        'CmdClearCache
+        '
+        Me.CmdClearCache.Location = New System.Drawing.Point(387, 211)
+        Me.CmdClearCache.Name = "CmdClearCache"
+        Me.CmdClearCache.Size = New System.Drawing.Size(73, 25)
+        Me.CmdClearCache.TabIndex = 21
+        Me.CmdClearCache.Text = "Clear Cache"
+        Me.CmdClearCache.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 298)
+        Me.Controls.Add(Me.CmdClearCache)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureTile)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ListMapServers)
+        Me.Controls.Add(Me.cmdGetTile)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtLongitude)
+        Me.Controls.Add(Me.txtLatitude)
+        Me.Controls.Add(Me.txtZoom)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        CType(Me.PictureTile, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListMapServers As ListBox
+    Friend WithEvents cmdGetTile As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtLongitude As TextBox
+    Friend WithEvents txtLatitude As TextBox
+    Friend WithEvents txtZoom As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureTile As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents CmdClearCache As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,247 @@
+﻿Imports TileServer  ' this is the DLL Interface
+Imports System.Reflection
+Imports System.IO
+
+
+Public Class Form1
+
+    Private myServer As IServer
+    Private myServers As New ArrayList
+    Private NoOfServers As Integer
+    Private myFolder As String = My.Application.Info.DirectoryPath
+    Private Const PI As Double = 3.14159265358979
+    Private TileExtension As String
+    Private maxZoom As Integer
+    Private TileName As String
+    Private FullTileName As String
+    Private TileDir As String
+    Private TileFolder As String
+    Private X As Integer
+    Private Y As Integer
+    Private Zoom As Integer
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Label5.Text = ""
+        Dim myAssembly As Assembly
+
+        Dim myFiles As String()
+        Dim Dll, DllBase As String
+
+        Dim myTypes As Type()
+        Dim myType As Type
+
+        ' The following statement gives a warning saying it is deprecated
+
+        ' AppDomain.CurrentDomain.AppendPrivatePath("Tiles")
+
+        ' Therefore, in order to load the dlls existing in subfolder Tiles
+        ' we need to declare that folder in App.config file. 
+
+        ' <runtime>
+        '  <assemblyBinding xmlns = "urn:schemas-microsoft-com:asm.v1" >
+        '    <probing privatePath="Tiles"/>
+        '          </assemblyBinding>
+        ' </runtime>
+
+        ' get all DLL files that are inside the subfolder Tiles
+        myFiles = Directory.GetFiles(myFolder & "\Tiles", "*.dll")
+
+        ' load all these DLLs and get their type (eg their classes). If they
+        ' implement the interface "TileServer.IServer" add these classes
+        ' to myServers array 
+
+        For Each Dll In myFiles
+            DllBase = Path.GetFileNameWithoutExtension(Dll)
+            myAssembly = Assembly.Load(DllBase)
+            myTypes = myAssembly.GetTypes
+            For Each myType In myTypes
+                If myType.GetInterface("IServer") IsNot Nothing Then
+                    If Not myType.IsAbstract Then
+                        myServers.Add(myType)
+                    End If
+                End If
+            Next
+        Next
+
+        NoOfServers = myServers.Count
+
+        ' place the name of the servers in a list box
+        Dim N As Integer
+        For N = 1 To NoOfServers
+            ListMapServers.Items.Add(myServers(N - 1).Name)
+        Next
+
+        ' by default select the first server (eg class) and create an instance of it
+        ListMapServers.SelectedIndex = 0
+        myServer = Activator.CreateInstance(myServers(ListMapServers.SelectedIndex))
+        TileExtension = myServer.ImageType  ' ".JPG" ".PNG" or whatever
+        maxZoom = myServer.MaximumZoom
+
+    End Sub
+
+    Private Sub CmdGetTile_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdGetTile.Click
+
+        ClearImage()
+
+        If DoCheck() = False Then
+            Exit Sub
+        End If
+
+        If My.Computer.FileSystem.FileExists(FullTileName) Then
+            PictureTile.Image = Image.FromFile(FullTileName)
+            Me.Text = TileName & " - from cache!"
+            Exit Sub
+        End If
+
+        Dim TempFile As String = myFolder & "\temp" & TileExtension
+
+        If myServer.DownloadTile(X, Y, Zoom, TempFile) Then
+            If Not My.Computer.FileSystem.DirectoryExists(TileFolder) Then
+                Directory.CreateDirectory(TileFolder)
+            End If
+            My.Computer.FileSystem.CopyFile(TempFile, FullTileName, True)
+            PictureTile.Image = Image.FromFile(FullTileName)
+            My.Computer.FileSystem.DeleteFile(TempFile)
+            Me.Text = TileName & " - from internet!"
+        Else
+            Label5.Text = "DOWNLOAD ERROR!"
+        End If
+
+    End Sub
+
+    Private Sub CmdClearCache_Click(sender As Object, e As EventArgs) Handles CmdClearCache.Click
+
+        ClearImage()
+
+        If DoCheck() = False Then
+            Exit Sub
+        End If
+
+        If My.Computer.FileSystem.FileExists(FullTileName) Then
+            My.Computer.FileSystem.DeleteFile(FullTileName)
+            Me.Text = ""
+        Else
+            Label5.Text = "NOT IN CACHE!"
+        End If
+
+    End Sub
+
+    Private Function XFromLon(ByVal lon As Double, ByVal Zoom As Integer) As Integer
+
+        Dim dXY As Double
+
+        dXY = PI / (2 ^ Zoom)
+        lon = PI + lon * PI / 180.0
+        XFromLon = Int(lon / dXY)
+
+    End Function
+
+    Private Function YFromLat(ByVal lat As Double, ByVal Zoom As Integer) As Integer
+
+        Dim dXY As Double
+
+        dXY = PI / (2 ^ Zoom)
+
+        lat = lat * PI / 360.0  ' equivalent to lat=lat/2
+        lat = lat + PI / 4.0
+        lat = Math.Tan(lat)
+        lat = Math.Log(lat)
+        lat = PI - lat
+        YFromLat = Int(lat / dXY)
+
+    End Function
+
+    Private Sub ListMapServers_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListMapServers.SelectedIndexChanged
+
+        myServer = Nothing
+        myServer = Activator.CreateInstance(myServers(ListMapServers.SelectedIndex))
+        TileExtension = myServer.ImageType  ' ".JPG" ".PNG" or whatever
+        maxZoom = myServer.MaximumZoom
+
+    End Sub
+
+    Private Function TileDirFromXYZ(ByVal X As Integer, ByVal Y As Integer, ByVal Z As Integer) As String
+
+        TileDirFromXYZ = ""
+        If Z < 4 Then Exit Function
+
+        Dim N As Integer
+        Dim Limit As Integer
+
+        For N = Z To 4 Step -1
+            Limit = 2 ^ N
+            If X < Limit Then
+                If Y < Limit Then
+                    TileDirFromXYZ = TileDirFromXYZ & "\0"
+                Else
+                    TileDirFromXYZ = TileDirFromXYZ & "\2"
+                    Y = Y - Limit
+                End If
+            Else
+                If Y < Limit Then
+                    TileDirFromXYZ = TileDirFromXYZ & "\1"
+                Else
+                    TileDirFromXYZ = TileDirFromXYZ & "\3"
+                    Y = Y - Limit
+                End If
+                X = X - Limit
+            End If
+        Next
+
+    End Function
+
+    Private Sub ClearImage()
+
+        If PictureTile.Image IsNot Nothing Then PictureTile.Image.Dispose()
+        PictureTile.Image = Nothing
+        Label5.Text = ""
+        Me.Text = ""
+        Me.Refresh()
+
+    End Sub
+
+    Private Function DoCheck() As Boolean
+
+        DoCheck = False
+
+        Try
+            Zoom = CInt(Val(txtZoom.Text))
+            If Zoom > maxZoom Then
+                Label5.Text = "ZOOM TOO LARGE!"
+                Exit Function
+            End If
+        Catch ex As Exception
+            Label5.Text = "CHECK ZOOM!"
+            Exit Function
+        End Try
+
+        Dim Lat, Lon As Double
+        Try
+            Lat = Val(txtLatitude.Text)
+            Lon = Val(txtLongitude.Text)
+            If (Lat > 89) Or (Lat < -89) Then
+                Label5.Text = "CHECK LAT!"
+                Exit Function
+            End If
+            If (Lon > 180) Or (Lon < -180) Then
+                Label5.Text = "CHECK LON!"
+                Exit Function
+            End If
+            X = XFromLon(Lon, Zoom)
+            Y = YFromLat(Lat, Zoom)
+        Catch ex As Exception
+            Label5.Text = "CHECK LAT/LON!"
+            Exit Function
+        End Try
+
+        TileFolder = myFolder & "\Tiles\" & myServer.ServerName
+        TileDir = TileDirFromXYZ(X, Y, Zoom)
+        TileName = "L" & Trim(Zoom) & "X" & Trim(X) & "Y" & Trim(Y) & TileExtension
+        FullTileName = TileFolder & "\" & TileDir & "\" & TileName
+
+        DoCheck = True
+
+    End Function
+
+End Class
